d437d481f058295afff865e85e648c4b618b4e45
Chris Brewin <cbrewin93@gmail.com>
2017-10-02 11:47:33 +1000

    Build of the angular repository for heroku

--- Server.js
+++ Server.js
@@ -5,8 +5,8 @@ const app = express();
 app.use(express.static(__dirname + '/dist'));
 //Set up routing
 app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname + '/dist/index.html'));
-  });
+    res.sendFile(path.join(__dirname + '/dist'));
+});
 // Start the app by listening on the default
 // Heroku port
 app.listen(process.env.PORT || 8080);
